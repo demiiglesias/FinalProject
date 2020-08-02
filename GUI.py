d410f05e94d889c47f2bd982e8481aa6a164d378
demiiglesias <51970368+demiiglesias@users.noreply.github.com>
2020-08-02 19:32:20 -0400

    GUI updates 8/2 7.29.pm

--- GUI.py
+++ GUI.py
@@ -1,10 +1,9 @@
 from tkinter import *
 import pandas as pd
 from test import *
-from nicksCode import *
+import nicksCode
+
 # from collaborativeFiltering import *
-
-
 # movies = pd.read_csv("movies.csv")
 # genres = movies['genres']
 #
@@ -14,6 +13,7 @@ from nicksCode import *
 root = Tk()
 root.title('Movie Recommendation Engine')
 root.geometry("700x500")
+root.configure(background='black')
 
 
 def content():
@@ -21,19 +21,24 @@ def content():
     top.title('Content Based Filtering')
     top.geometry("700x500")
     m = Label(top, text='Enter a favorite movie: ', font="Verdana 15")
-    user_fave_movie = Entry(top, width=30)
     m.place(x=200, y=100)
+    user_fave_movie = Entry(top, width=30)
     user_fave_movie.place(x=200, y=140)
+    user_fave_movie.focus()
 
     def entered():
-        user_input = Label(top, text=user_fave_movie.get())
-
-        # my_label.pack()
-        test = improved_recommendations(user_fave_movie)
-        test1 = Label(top, text=test).pack()
+        m.forget()
+        title = Label(top, text='We think you will love these movies too!', font="Verdana 15", border="2")
+        title.place(x=200, y=40)
+        user_input = user_fave_movie.get()
+        recommendations = nicksCode.improved_recommendations(str(user_input))
+        recommendations_display = Label(top, text=recommendations)
+        recommendations_display.place(x=240, y=140)
+        user_fave_movie.destroy()
+        enter.destroy()
 
     enter = Button(top, text='Enter', command=entered)
-    enter.place(x=200, y=190)
+    enter.place(x=200, y=180)
 
     top.mainloop()
 
@@ -88,10 +93,6 @@ def collaborative():
     western = StringVar()
     chk = Checkbutton(c, text="Western", variable=western).place(x=60, y=360)
 
-    test = Person("demi", 79)
-    label_test = Label(c, text=Person("Demi", 28))
-    label_test.pack()
-
     def show():
         my_label = Label(c, text="Genre")
         my_label.pack()
@@ -99,13 +100,16 @@ def collaborative():
     c.mainloop()
 
 
-first_Label = Label(root, text="Welcome to the Movie Recommendation System", font="Verdana 20")
-second_Label = Label(root, text="choose an algorithm for a movie recommendation", font="Verdana 20")
-collaborative_Button = Button(root, text="Collaborative Filtering", font="Verdana 15", command=collaborative)
-content_Button = Button(root, text="Content Filtering", font="Verdana 15", command=content)
+ct_Button = PhotoImage(file='content.png')
+co_Button = PhotoImage(file='collab.png')
+first_Label = Label(root, text="Welcome to the Movie Recommendation System", font="Verdana 20", fg="white", bg="black")
+second_Label = Label(root, text="choose an algorithm for a movie recommendation", font="Verdana 20", fg="white",
+                     bg="black")
+collaborative_Button = Button(root, image=co_Button, command=collaborative, borderwidth=0, bg="black")
+content_Button = Button(root, image=ct_Button, command=content, borderwidth=0)
 
 first_Label.place(x=130, y=70)
 second_Label.place(x=120, y=100)
-collaborative_Button.pack(padx=60, pady=20, side=LEFT)
-content_Button.pack(padx=60, pady=20, side=RIGHT)
+collaborative_Button.place(x=210, y=180)
+content_Button.place(x=210, y=270)
 root.mainloop()
